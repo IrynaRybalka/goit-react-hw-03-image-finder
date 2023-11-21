1e6bee4911c2ac7055fef79fc37a287658784fc2
IrynaRybalka <129180240+IrynaRybalka@users.noreply.github.com>
2023-11-21 21:56:39 +0200

    updated

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -5,22 +5,26 @@ import { Image } from './ImageGalleryItem.styled';
 export class ImageGalleryItem extends Component {
   state = { isModalOpen: false };
 
-  openModal = () => {
-    this.setState({ isModalOpen: true });
+  toggleModal = () => {
+    this.setState(({ isModalOpen }) => ({ isModalOpen: !isModalOpen }));
   };
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
+
+  // openModal = () => {
+  //   this.setState({ isModalOpen: true });
+  // };
+  // closeModal = () => {
+  //   this.setState({ isModalOpen: false });
+  // };
 
   render() {
     const { smallImg, largeImg } = this.props;
     return (
       <div>
-        <Image src={smallImg} alt="pic" onClick={this.openModal} />
+        <Image src={smallImg} alt="pic" onClick={this.toggleModal} />
         <ModalContainer
           url={largeImg}
           isModalOpen={this.state.isModalOpen}
-          closeModal={this.closeModal}
+          closeModal={this.toggleModal}
         />
       </div>
     );
